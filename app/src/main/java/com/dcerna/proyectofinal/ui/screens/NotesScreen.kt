def5a6bccb6b9cb0bc528ec34891be83144ed5a8
DT3264 <cernadaniel32@gmail.com>
2021-10-25 15:38:19 -0500

    Agregada primera version de pagina inicial

--- app/src/main/java/com/dcerna/proyectofinal/ui/screens/NotesScreen.kt
+++ app/src/main/java/com/dcerna/proyectofinal/ui/screens/NotesScreen.kt
@@ -2,14 +2,16 @@ package com.dcerna.proyectofinal.ui.screens
 
 import android.content.Context
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
@@ -20,36 +22,45 @@ import com.dcerna.proyectofinal.data.Multimedia
 import com.dcerna.proyectofinal.data.Nota
 import com.dcerna.proyectofinal.data.NotasBD
 import com.dcerna.proyectofinal.data.Recordatorio
+import java.util.*
 
 @Composable
 fun NotesScreen(navController: NavController) {
-    var context = LocalContext.current
-    var db = NotasBD.getInstance(context)
+    val context = LocalContext.current
+    val db = NotasBD.getInstance(context)
     var notas = db.DAONotas().getNotas()
-    LazyColumn(modifier = Modifier.fillMaxHeight()) {
-        item {
-            GetButton(noteID = "a", text = "", navController = navController)
+
+    val dialogAgregar = remember { mutableStateOf(false) }
+
+    if (dialogAgregar.value) {
+        MuestraDialogAgregar(dialogAgregar, navController)
+    }
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { dialogAgregar.value = true}) {
+                Icon(Icons.Filled.Add, null)
+            }
         }
-        items(notas) {
-            GetButton(
-                it.idNota.toString(),
-                "${it.idNota},${it.titulo},${it.descripcion}",
-                navController
-            )
+    ) {
+        LazyColumn(modifier = Modifier.fillMaxHeight()) {
+            items(notas) {
+                GetNota(
+                    it.idNota.toString(),
+                    "${it.idNota},${it.titulo},${it.descripcion}",
+                    navController,
+                )
+            }
         }
     }
 }
 
 @Composable
-fun GetButton(noteID: String, text: String, navController: NavController) {
-    var context = LocalContext.current
-
+fun GetNota(noteID: String, text: String, navController: NavController) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(15.dp)
             .clickable {
-                addNote(context)
                 navController.navigate(route = "noteDetails/$noteID")
             },
         elevation = 10.dp
@@ -58,35 +69,59 @@ fun GetButton(noteID: String, text: String, navController: NavController) {
     }
 }
 
-fun addNote(context: Context) {
-    var db = NotasBD.getInstance(context)
-    db.DAONotas().save(
-        Nota(
-            descripcion = "Test",
-            fechaCreacion = 0,
-            titulo = "Titulo prueba",
-            nota = "Esta es una nota"
-        )
+@Composable
+fun MuestraDialogAgregar(dialogState: MutableState<Boolean>, navController: NavController) {
+    val context = LocalContext.current
+    AlertDialog(
+        onDismissRequest = {
+            dialogState.value = false
+        },
+        title = {
+            Text(text = "Agregar")
+        },
+        text = {
+            Column {
+                Text("¿Qué desea agregar?")
+            }
+        },
+        buttons = {
+            Column(
+                modifier = Modifier.padding(all = 8.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                        val idNuevaNota = insertaNota(context)
+                        dialogState.value = false
+                        navController.navigate(route = "noteDetails/$idNuevaNota")
+
+                    }
+                ) {
+                    Text("Nota")
+                }
+                Spacer(modifier = Modifier.size(16.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                        val idNuevaNota = insertaNota(context, esTarea = true)
+                        dialogState.value = false
+                        navController.navigate(route = "noteDetails/$idNuevaNota")
+                    }
+                ) {
+                    Text("Tarea")
+                }
+            }
+        }
     )
-    var notas = db.DAONotas().getNotas()
-    var lastNote = notas.last()
-    var recordatorio = Recordatorio(
-        idNota = lastNote.idNota,
-        fechaRecordatorio = 10
-    )
-    println("Agregando recordatorio")
-    println("${recordatorio.idNota},${recordatorio.idRecordatorio},${recordatorio.fechaRecordatorio}")
-    db.DAONotas().save(
-        recordatorio
-    )
-    var multimedia =
-        Multimedia(
-            idNota = lastNote.idNota,
-            descripcion = "Mel",
-            ruta = "sdcard/",
-            tipo = "mp3"
-        )
-    println("Agregando multimedia")
-    println("${multimedia.idNota},${multimedia.idMultimedia},${multimedia.ruta},${multimedia.tipo},${multimedia.descripcion}")
-    db.DAONotas().save(multimedia)
+}
+
+fun insertaNota(context: Context, esTarea: Boolean = false): Long {
+    val tiempoActual = Calendar.getInstance().timeInMillis
+    val nota = Nota(esTarea = esTarea, fechaCreacion = tiempoActual)
+    val db = NotasBD.getInstance(context)
+    db.DAONotas().save(nota)
+
+    // La nota más reciente es la última en la lista
+    return db.DAONotas().getNotas().last().idNota
 }
